7358267962fa476e28e71e25c025efd49ce1b74c
Juraj Vlk <juraj.vlk@gmail.com>
2015-01-31 00:56:03 +0100

    initial commit

---
+++ src/main/java/cz/moro/test/App.java
@@ -0,0 +1,13 @@
+package cz.moro.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
